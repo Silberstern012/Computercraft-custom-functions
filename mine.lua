86ca58075b235bbb555020e3bc4c61bbad4c3eaf
Silverstar <147107851+Silberstern012@users.noreply.github.com>
2026-09-24 01:35:26 +0200

    Refactor getRotation function in mine.lua
    Refactor getRotation function to use local function syntax and fix variable assignment.

--- mine.lua
+++ mine.lua
@@ -1,11 +1,12 @@
-local getRotation()
-    startPos = gps.locate()
+local function getRotation()
+    startPos.x,startPos.y,startPos.z = gps.locate()
     if turtle.detect() then
         turtle.back()
         Pos2 = gps.locate()
         turtle.forward()
         
         neg_1 = {}
+        print(startPos)
         for num, pos in pairs(startPos) do
             if number<0 then neg_1[num] = true
             else neg_1[num] = false end  
@@ -20,6 +21,7 @@ local getRotation()
     
     end
 
+end
 
 local function goto(pos)
 
