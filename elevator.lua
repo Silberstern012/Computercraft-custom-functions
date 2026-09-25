0c2e4efaa2c8fbe6efbdb131bcf3463a2b7b285c
Silverstar <147107851+Silberstern012@users.noreply.github.com>
2026-09-25 14:53:40 +0200

    Refactor elevator logic and improve logging
    Removed unnecessary print statement and added a log for setting the target floor.

--- elevator.lua
+++ elevator.lua
@@ -42,11 +42,11 @@ function main()
         playerData = textutils.unserialiseJSON(f.getPlayerData())
         for pamt, player in ipairs(playerData.players) do
             for famt, floorypos in pairs(floory) do
-                print(famt)
                 floorypos2 = floorypos + 5
                 if arrived and tu and elevator.getCurrentY() ~= floorypos and f.isInArea(player.x, player.y, player.z,coordx1,floorypos,coordz1,coordx2,floorypos2,coordz2) then
                     elevator.setTargetFloor(floorypos)
                     arrived = false
+                    print("Level set to "..floorypos)
                 end
             end
         end
